c214e1183e15dc3aaeab8e8a274bd08f2ba28514
radoslav <radoslav@DESKTOP-45F398S>
2020-04-21 18:35:31 +0300

    Edited Gossip DTO

--- src/main/java/bg/codeacademy/spring/gossiptalks/dto/GossipDto.java
+++ src/main/java/bg/codeacademy/spring/gossiptalks/dto/GossipDto.java
@@ -5,11 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
